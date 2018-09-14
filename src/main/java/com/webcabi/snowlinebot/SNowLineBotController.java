096b8f6772e2c8e71b9717af7ec23ccf6a059fe1
yonezawahr <yonezawahr@gmail.com>
2018-09-14 14:23:23 +0900

    Add Message Endpoint

--- src/main/java/com/webcabi/snowlinebot/SNowLineBotController.java
+++ src/main/java/com/webcabi/snowlinebot/SNowLineBotController.java
@@ -100,10 +100,66 @@ public class SNowLineBotController {
         }
     }
 
+	@RequestMapping(value = "/message",
+			        method = RequestMethod.POST,
+			        consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void sendMessage(@RequestBody MessageRequest request) throws Exception {
+		List<String> users = new ArrayList<String>() {
+			private static final long serialVersionUID = 1L;
+
+			{
+				add("Uef8ec22beca510e31a4b9130df7830db");
+				add("U096871bbdcf5f3762d56f9e812bba482");
+				add("U80f7dbdf2a6b27a95b3b522261b779df");
+				add("U7d53343510f879ec9f655efbce371f2d");
+				add("U1e55466292f8c6abfb15ed30b127bb72");
+			}
+		};
+		
+        try {
+            for (String user : users) {
+        		lineMessagingClient.pushMessage(new PushMessage(user,
+        				                        new TextMessage(request.getMessage())
+        				                       	)).get();
+        	}
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 	@RequestMapping(value = "/invite",
 					method = RequestMethod.POST,
 					consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void sendMessage(@RequestBody InviteRequest request) throws Exception {
+		List<String> users = new ArrayList<String>() {
+			private static final long serialVersionUID = 1L;
+			{
+				add("Uef8ec22beca510e31a4b9130df7830db");
+				add("U096871bbdcf5f3762d56f9e812bba482");
+				add("U80f7dbdf2a6b27a95b3b522261b779df");
+				add("U7d53343510f879ec9f655efbce371f2d");
+				add("U1e55466292f8c6abfb15ed30b127bb72");
+			}
+		};
+
+		try {
+			for (String user : users) {
+				lineMessagingClient.pushMessage(new PushMessage(user,
+					new TemplateMessage("對間さんから焼肉の招待(" + request.getDatetime() + ")が届きました！ぜひ参加してね！！",
+						new ConfirmTemplate("對間さんから焼肉の招待(" + request.getDatetime() + ")が届きました！ぜひ参加してね！！参加しますか？",
+						new MessageAction("はい", "はい"),
+						new MessageAction("いいえ", "いいえ")
+				)))).get();
+			}
+		} catch (InterruptedException | ExecutionException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@RequestMapping(value = "/test",
+					method = RequestMethod.POST,
+					consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void sendTest(@RequestBody InviteRequest request) throws Exception {
 		List<String> users = new ArrayList<String>() {
 			private static final long serialVersionUID = 1L;
 			{
