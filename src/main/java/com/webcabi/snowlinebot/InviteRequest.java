d86fcbe1ead43fa04ce70a70566a0d3ee7574b9b
yonezawahr <yonezawahr@gmail.com>
2018-09-14 13:43:18 +0900

    Add Message Endpoint

---
+++ src/main/java/com/webcabi/snowlinebot/InviteRequest.java
@@ -0,0 +1,13 @@
+package com.webcabi.snowlinebot;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InviteRequest implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String datetime;
+}
