be59ae71eab566d2518049eee07bb8eb4353b1f8
nobutnk <nobutnk@gmail.com>
2018-09-14 14:50:25 +0900

    焼肉食べたいから始まる通知を実装

--- src/main/java/com/webcabi/snowlinebot/service/NotifyMessageHandler.java
+++ src/main/java/com/webcabi/snowlinebot/service/NotifyMessageHandler.java
@@ -5,20 +5,24 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.model.response.BotApiResponse;
-import com.linecorp.bot.client.LineMessagingClient;
 
 @Service
 public class NotifyMessageHandler {
 	
 	private final LineMessagingClient lineMessagingClient;
+	
+	@Value("${yakiniku.url}")
+	private String yakinikuUrl;
 	
 	public NotifyMessageHandler(LineMessagingClient lineMessagingClient) {
 
@@ -32,7 +36,9 @@ public class NotifyMessageHandler {
         List<Message> messages = null;
 		switch (receivedMessage) {
 		case "焼肉食べたい":
-				messages = Arrays.asList(new TextMessage("イイね！"));
+				messages = Arrays.asList(
+						new TextMessage("イイね！"),
+						new TextMessage(yakinikuUrl));
 		}
 		
 		return lineMessagingClient
